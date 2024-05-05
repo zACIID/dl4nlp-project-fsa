7bf48792be1581eb820381f1d3ef52e5f7ae47fb
zACIID <marchioropierluigi@gmail.com>
2024-05-05 16:08:30 +0200

    feat: now loss is just negative cosine_similarity

--- src/models/loss_functions.py
+++ src/models/loss_functions.py
@@ -7,9 +7,9 @@ def custom_regression_loss(
         y_true: torch.Tensor,
         y_pred: torch.Tensor,
 ) -> torch.Tensor:
-    loss = (-cosine_similarity(y_true, y_pred, dim=0) + 1) + l1_loss(y_true, y_pred)
-    # loss = -cosine_similarity(y_true, y_pred, dim=-1)
+    # loss = (-cosine_similarity(y_true, y_pred, dim=0) + 1) + l1_loss(y_true, y_pred)
     # loss = mse_loss(y_true, y_pred)
+    loss = -cosine_similarity(y_true, y_pred, dim=-1)
 
     return loss
 
