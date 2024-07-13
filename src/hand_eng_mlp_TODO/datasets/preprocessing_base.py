bd05f46962404d84a333eb32fc3acacdd38453cf
RuieX <runjiexia28@gmail.com>
2024-07-14 01:42:24 +0200

    Update preprocessing_base.py
    the merge reverted it to before the update

--- src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
+++ src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
@@ -1,5 +1,5 @@
 import typing
-
+import pandas as pd
 import pyspark.sql as psql
 from loguru import logger
 from pyspark.sql import types as psqlt, functions as psqlf
@@ -10,8 +10,9 @@ from transformers import AutoTokenizer, BatchEncoding
 import data.spark as S
 import data.stocktwits_crypto_dataset as sc
 import data.common as common
-import hand_eng_mlp.datasets_TODO.preprocessing_features_extraction as ppfe
+import hand_eng_mlp_TODO.datasets.preprocessing_features_extraction as ppfe
 import utils.io as io_
+from custom_features import CustomFeatures
 
 
 # TODO ( ͡° ͜ʖ ͡°) compile once decided (in trial)
@@ -31,25 +32,25 @@ PROCESSED_DATASET_SCHEMA: psqlt.StructType = (
 
 
 # Define UDFs for feature extraction functions
-compute_sentence_polarity_VADER_udf = udf(ppfe.compute_sentence_polarity_VADER, FloatType())
+compute_sentence_polarity_VADER_udf = udf(ppfe.compute_vader_polarity, FloatType())
 calculate_pos_neg_features_VADER_udf = udf(
-    ppfe.calculate_pos_neg_features_VADER,
+    ppfe.calculate_vader_pos_neg_features,
     StructType([
         StructField("Pos_Neg_Ratio_VADER", FloatType()),
         StructField("Pos_Neg_Difference_VADER", FloatType())
     ])
 )
-calculate_sentiment_entropy_VADER_udf = udf(ppfe.calculate_sentiment_entropy_VADER, FloatType())
+calculate_sentiment_entropy_VADER_udf = udf(ppfe.calculate_vader_sentiment_entropy, FloatType())
 calculate_pos_neg_features_SN_udf = udf(
-    ppfe.calculate_pos_neg_features_SN,
+    ppfe.calculate_sentic_pos_neg_features,
     StructType([
         StructField("Pos_Neg_Ratio_SenticNet", FloatType()),
         StructField("Pos_Neg_Difference_SenticNet", FloatType())
     ])
 )
-compute_sentence_polarity_SWN_udf = udf(ppfe.compute_sentence_polarity_SWN, FloatType())
+compute_sentence_polarity_SWN_udf = udf(ppfe.compute_swn_polarity, FloatType())
 emotion_recognition_SN_udf = udf(
-    ppfe.emotion_recognition_SN,
+    ppfe.sentic_emotion_recognition,
     StructType([
         StructField("INTROSPECTION", FloatType()),
         StructField("TEMPER", FloatType()),
@@ -65,7 +66,7 @@ calculate_readability_metrics_udf = udf(
         StructField("coleman_liau_index", FloatType())
     ])
 )
-"""compute_overall_sentiment_features_udf = udf(
+compute_overall_sentiment_features_udf = udf(
     ppfe.compute_overall_sentiment_features,
     StructType([
         StructField("overall_valence_mean", FloatType()),
@@ -78,7 +79,7 @@ calculate_readability_metrics_udf = udf(
         StructField("arousal_contrast", FloatType()),
         StructField("dominance_contrast", FloatType())
     ])
-)"""
+)
 
 
 # Function to compute the additional features
@@ -87,6 +88,8 @@ def get_new_features(df: psql.DataFrame) -> psql.DataFrame:
     :param df: Spark DataFrame with text data
     :return: DataFrame with additional computed features
     """
+    features = CustomFeatures()
+
     # Sentiment score with VADER, SenticNet and SentiWordNet
     df = df.withColumn('sentiment_score_VADER', compute_sentence_polarity_VADER_udf(df['text']))
 
@@ -116,8 +119,7 @@ def get_new_features(df: psql.DataFrame) -> psql.DataFrame:
     df = df.withColumn('coleman_liau_index', readability_metrics_udf['coleman_liau_index'])
 
     # Lexical Affect Features: Valence, Arousal, Dominance (VAD)
-    sentiment_dataset_path = io_.DATA_DIR / "nomedelfileTODO"  # TODO vedere se epossible scaricare on the spot
-    sentiment_data, mean_medians = ppfe.load_sentiment_dataset(sentiment_dataset_path)
+    sentiment_data, mean_medians = ppfe.load_sentiment_dataset(io_.DATA_DIR)
     oa_sent_features_udf = compute_overall_sentiment_features_udf(df['text'], lit(sentiment_data), lit(mean_medians))
     df = df.withColumn('overall_valence_mean', oa_sent_features_udf['overall_valence_mean'])
     df = df.withColumn('overall_arousal_mean', oa_sent_features_udf['overall_arousal_mean'])
@@ -130,34 +132,6 @@ def get_new_features(df: psql.DataFrame) -> psql.DataFrame:
     df = df.withColumn('dominance_contrast', oa_sent_features_udf['dominance_contrast'])
 
     return df
-
-# pandas og version
-# # Function to compute the additional features
-# def get_new_features(df):
-#     # Sentiment score with VADER, SenticNet and SentiWordNet
-#     df['sentiment_score_VADER'] = df['text'].apply(ppfe.compute_sentence_polarity_VADER)
-#     df['Pos_Neg_Ratio_VADER'], df['Pos_Neg_Difference_VADER'] = zip(*df['text'].apply(ppfe.calculate_pos_neg_features_VADER))
-#     df['Sentiment_Entropy_VADER'] = df['text'].apply(ppfe.calculate_sentiment_entropy_VADER)
-#     df['Pos_Neg_Ratio_SenticNet'], df['Pos_Neg_Difference_SenticNet'] = zip(*df['text'].apply(ppfe.calculate_pos_neg_features_SN))
-#     df['sentiment_score_SWN'] = df['text'].apply(ppfe.compute_sentence_polarity_SWN)
-#
-#     # Emotion recognition
-#     df['emotion_features'] = df['text'].apply(ppfe.emotion_recognition_SN)
-#     df[['INTROSPECTION', 'TEMPER', 'ATTITUDE', 'SENSITIVITY']] = df['emotion_features'].apply(pd.Series)
-#     df.drop(columns=['emotion_features'], inplace=True)
-#
-#     # Readability metrics
-#     df['flesch_kincaid_grade'], df['gunning_fog'], df['coleman_liau_index'] = zip(*df['text'].apply(ppfe.calculate_readability_metrics))
-#
-#     # Lexical Affect Features: Valence, Arousal, Dominance (VAD)
-#     sentiment_dataset_path = 'BRM-emot-submit.csv'
-#     sentiment_data, mean_medians = ppfe.load_sentiment_dataset(sentiment_dataset_path)
-#     df['overall_valence_mean'], df['overall_arousal_mean'], df['overall_dominance_mean'], df['overall_valence_std'], df['overall_arousal_std'], df['overall_dominance_std'], df['valence_contrast'], df['arousal_contrast'], df['dominance_contrast'] = zip(*df['text'].apply(ppfe.compute_overall_sentiment_features, sentiment_data=sentiment_data, default_mean_value=mean_medians))
-#
-#     # Convert each element in the DataFrame to float if possible
-#     df = df.applymap(lambda x: float(x) if isinstance(x, (int, float)) else x)
-#
-#     return df
 
 
 def preprocess_dataset(
