c2a09d1e7dcf3c23f49d6f6913804d742446fecb
RuieX <runjiexia28@gmail.com>
2024-07-15 22:21:55 +0200

    Update preprocessing_base.py

--- src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
+++ src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
@@ -143,7 +143,10 @@ def get_new_features(df: psql.DataFrame, text_col: str) -> psql.DataFrame:
     print("END step7")
 
     # Lexical Affect Features: Valence, Arousal, Dominance (VAD)
+    print("START load VAD dataset")
     sentiment_data, mean_medians = ppfe.load_sentiment_dataset(io_.DATA_DIR)
+    print("END load VAD dataset")
+
     df = df.withColumn("lexical_affect_features", compute_overall_sentiment_features_udf(df[text_col], sentiment_data, mean_medians))
     df = df.select(
         "*",
