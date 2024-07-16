19579256b4e8f7b5d408e08db57ecba4945c57d1
RuieX <runjiexia28@gmail.com>
2024-07-16 12:11:08 +0200

    debugging

--- src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
+++ src/hand_eng_mlp_TODO/datasets/preprocessing_base.py
@@ -99,16 +99,23 @@ def get_new_features(df: psql.DataFrame, text_col: str) -> psql.DataFrame:
     # Sentiment score with VADER, SenticNet and SentiWordNet
     df = df.withColumn('sentiment_score_VADER', compute_sentence_polarity_VADER_udf(df[text_col]))
 
-    df = df.withColumn("vader_features", calculate_pos_neg_features_VADER_udf(df[text_col]))
-    df = df.select(
-        "*",
-        df["vader_features"]["Pos_Neg_Ratio_VADER"].alias("Pos_Neg_Ratio_VADER"),
-        df["vader_features"]["Pos_Neg_Difference_VADER"].alias("Pos_Neg_Difference_VADER")
-    ).drop("vader_features")
-    print("END step2")
 
-    df = df.withColumn('Sentiment_Entropy_VADER', calculate_sentiment_entropy_VADER_udf(df[text_col]))
-    print("END step3")
+
+
+    # df = df.withColumn("vader_features", calculate_pos_neg_features_VADER_udf(df[text_col]))
+    # df = df.select(
+    #     "*",
+    #     df["vader_features"]["Pos_Neg_Ratio_VADER"].alias("Pos_Neg_Ratio_VADER"),
+    #     df["vader_features"]["Pos_Neg_Difference_VADER"].alias("Pos_Neg_Difference_VADER")
+    # ).drop("vader_features")
+    # print("END step2")
+
+
+
+    # df = df.withColumn('Sentiment_Entropy_VADER', calculate_sentiment_entropy_VADER_udf(df[text_col]))
+    # print("END step3")
+
+
 
     # df = df.withColumn("senticnet_features", calculate_pos_neg_features_SN_udf(df[text_col]))
     # df = df.select(
@@ -118,29 +125,33 @@ def get_new_features(df: psql.DataFrame, text_col: str) -> psql.DataFrame:
     # ).drop("senticnet_features")
     # print("END step4")
 
-    df = df.withColumn('sentiment_score_SWN', compute_sentence_polarity_SWN_udf(df[text_col]))
-    print("END step5")
+
+
+    # df = df.withColumn('sentiment_score_SWN', compute_sentence_polarity_SWN_udf(df[text_col]))
+    # print("END step5")
+
+
 
     # Emotion recognition
-    df = df.withColumn("emotion_recognition", emotion_recognition_SN_udf(df[text_col]))
-    df = df.select(
-        "*",
-        df["emotion_recognition"]["INTROSPECTION"].alias("INTROSPECTION"),
-        df["emotion_recognition"]["TEMPER"].alias("TEMPER"),
-        df["emotion_recognition"]["ATTITUDE"].alias("ATTITUDE"),
-        df["emotion_recognition"]["SENSITIVITY"].alias("SENSITIVITY")
-    ).drop("emotion_recognition")
-    print("END step6")
+    # df = df.withColumn("emotion_recognition", emotion_recognition_SN_udf(df[text_col]))
+    # df = df.select(
+    #     "*",
+    #     df["emotion_recognition"]["INTROSPECTION"].alias("INTROSPECTION"),
+    #     df["emotion_recognition"]["TEMPER"].alias("TEMPER"),
+    #     df["emotion_recognition"]["ATTITUDE"].alias("ATTITUDE"),
+    #     df["emotion_recognition"]["SENSITIVITY"].alias("SENSITIVITY")
+    # ).drop("emotion_recognition")
+    # print("END step6")
 
-    # Readability metrics
-    df = df.withColumn("readability_metrics", calculate_readability_metrics_udf(df[text_col]))
-    df = df.select(
-        "*",
-        df["readability_metrics"]["flesch_kincaid_grade"].alias("flesch_kincaid_grade"),
-        df["readability_metrics"]["gunning_fog"].alias("gunning_fog"),
-        df["readability_metrics"]["coleman_liau_index"].alias("coleman_liau_index")
-    ).drop("readability_metrics")
-    print("END step7")
+    # # Readability metrics
+    # df = df.withColumn("readability_metrics", calculate_readability_metrics_udf(df[text_col]))
+    # df = df.select(
+    #     "*",
+    #     df["readability_metrics"]["flesch_kincaid_grade"].alias("flesch_kincaid_grade"),
+    #     df["readability_metrics"]["gunning_fog"].alias("gunning_fog"),
+    #     df["readability_metrics"]["coleman_liau_index"].alias("coleman_liau_index")
+    # ).drop("readability_metrics")
+    # print("END step7")
 
     # # Lexical Affect Features: Valence, Arousal, Dominance (VAD)
     # # print("START load VAD dataset")  #TODO: remove later, ask ruie
