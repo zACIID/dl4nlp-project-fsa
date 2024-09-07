ff65b9dce79999a7273c241739e99988d1bbd5f4
zACIID <marchioropierluigi@gmail.com>
2024-09-07 15:40:06 +0200

    fix: now properly loading .env and senticnet api keys in preprocessing_features_extraction.py

--- src/hand_eng_mlp_TODO/datasets/preprocessing_features_extraction.py
+++ src/hand_eng_mlp_TODO/datasets/preprocessing_features_extraction.py
@@ -3,6 +3,7 @@ import re
 from collections import Counter
 from typing import Dict, Tuple, Union
 
+import dotenv
 import nltk
 import pandas as pd
 import requests
@@ -16,11 +17,14 @@ from urllib3.util.retry import Retry
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 import hand_eng_mlp_TODO.datasets.custom_features as cf
+import utils.io as io_
 from utils.custom_features_utils import download_and_extract_zip
 
 # The following two packages have been added to pyproject.toml
 # poetry add vaderSentiment
 # poetry add textstat
+
+dotenv.load_dotenv(io_.PROJECT_ROOT / ".env")
 
 SENTICNET_API_EMOTION_KEY = os.getenv('SENTICNET_API_EMOTION_KEY')
 SENTICNET_API_POLARITY_KEY = os.getenv('SENTICNET_API_POLARITY_KEY')
