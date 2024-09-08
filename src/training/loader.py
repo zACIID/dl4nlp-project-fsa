4d12a9b1894a0d2517bd760717fa738a4d4291a3
zACIID <marchioropierluigi@gmail.com>
2024-09-08 02:21:53 +0200

    fix: now models are correctly loaded in pretrained/not-pretrained mode

--- src/training/loader.py
+++ src/training/loader.py
@@ -1,3 +1,4 @@
+import collections
 import typing
 
 import mlflow
@@ -36,16 +37,19 @@ def _load_finbert_model_and_data_module(
         dataset_choice: Dataset,
         dm_init_args: typing.Mapping[str, typing.Any]
 ) -> typing.Tuple[LightningModule, LightningDataModule]:
-    model = FineTunedFinBERT(**model_init_args)
-
+    # TODO this is janky at best, because it relies on the fact that a) hyperopt is the only place that
+    #   instantiates the pretrained models for training and b) that the search space, i.e. args passed,
+    #   is defined in such a way that only non-architecture-altering params are provided
     if env.should_hyperopt_on_pretrained_model():
-        best_model = load_best_model(Model.FINBERT, init_kwargs={
-            "strict": False,
-            "log_hparams": False
-        })
-        # I am loading the state dict and not keeping directly the best model because
-        #   I need to init the model with the provided model_init_args
-        model.load_state_dict(state_dict=best_model.state_dict(), strict=False)
+        model = load_best_model(Model.FINBERT, init_kwargs=collections.ChainMap(
+            model_init_args,
+            {
+                "strict": False,
+                "log_hparams": False
+            }
+        ))
+    else:
+        model = FineTunedFinBERT(**model_init_args)
 
     match dataset_choice:
         case Dataset.SC_TRAIN_VAL:
@@ -67,40 +71,42 @@ def _load_model_beijin_and_data_module(
         dataset_choice: Dataset,
         dm_init_args: typing.Mapping[str, typing.Any]
 ) -> typing.Tuple[LightningModule, LightningDataModule]:
-
-    # NOTE: need to specify default values because the full-training script
-    #   loads the model without passing any init keyword basically, the reason
-    #   being that those from the best model are later used
-    data: typing.Dict[str, typing.Any] = {
-        "n_layers": model_init_args.pop("n_layers", 10),
-        "dropout": model_init_args.pop("dropout", 0.2),
-        "linear": model_init_args.pop("linear", False),
-        "layernorm": model_init_args.pop("layernorm", True)
-    }
-
-    # The 0, 1, 2 here is not defined by the enum but by the hyperopt search space
-    if model_init_args["model_spec"] == 0:
-        model_init_args["model_spec"] = MLPType.BOX
-    elif model_init_args["model_spec"] == 1:
-        model_init_args["model_spec"] = MLPType.RHOMBOID
-        data["beta"] = model_init_args.pop("beta", 0.5)
-    elif model_init_args["model_spec"] == 2:
-        model_init_args["model_spec"] = MLPType.REPRHOMBOID
-        data["beta"] = model_init_args.pop("beta", 0.5)
-    else:
-        raise ValueError(f'Unknown model spec {model_init_args["model_spec"]}')
-
-    model_init_args["MLP_args"] = data
-    model = ModelBeijin(**model_init_args)
-
+    # TODO this is janky at best, because it relies on the fact that a) hyperopt is the only place that
+    #   instantiates the pretrained models for training and b) that the search space, i.e. args passed,
+    #   is defined in such a way that only non-architecture-altering params are provided
     if env.should_hyperopt_on_pretrained_model():
-        best_model = load_best_model(Model.HAND_ENG_MLP, init_kwargs={
-            "strict": False,
-            "log_hparams": False
-        })
-        # I am loading the state dict and not keeping directly the best model because
-        #   I need to init the model with the provided model_init_args
-        model.load_state_dict(state_dict=best_model.state_dict(), strict=False)
+        model = load_best_model(Model.HAND_ENG_MLP, init_kwargs=collections.ChainMap(
+            model_init_args,
+            {
+                "strict": False,
+                "log_hparams": False
+            }
+        ))
+    else:
+        # NOTE: need to specify default values because the full-training script
+        #   loads the model without passing any init keyword basically, the reason
+        #   being that those from the best model are later used
+        data: typing.Dict[str, typing.Any] = {
+            "n_layers": model_init_args.pop("n_layers", 10),
+            "dropout": model_init_args.pop("dropout", 0.2),
+            "linear": model_init_args.pop("linear", False),
+            "layernorm": model_init_args.pop("layernorm", True)
+        }
+
+        # The 0, 1, 2 here is not defined by the enum but by the hyperopt search space
+        if model_init_args["model_spec"] == 0:
+            model_init_args["model_spec"] = MLPType.BOX
+        elif model_init_args["model_spec"] == 1:
+            model_init_args["model_spec"] = MLPType.RHOMBOID
+            data["beta"] = model_init_args.pop("beta", 0.5)
+        elif model_init_args["model_spec"] == 2:
+            model_init_args["model_spec"] = MLPType.REPRHOMBOID
+            data["beta"] = model_init_args.pop("beta", 0.5)
+        else:
+            raise ValueError(f'Unknown model spec {model_init_args["model_spec"]}')
+
+        model_init_args["MLP_args"] = data
+        model = ModelBeijin(**model_init_args)
 
     match dataset_choice:
         case Dataset.SC_TRAIN_VAL:
@@ -142,18 +148,21 @@ def _load_end_to_end_model_and_data_module(
             raise ValueError(f'Unknown dataset {dataset_choice}')
 
 
-def load_best_model(model: Model, **init_kwargs) -> LightningModule:
+def load_best_model(model: Model, load_pretrained_on_sc: bool = True, **init_kwargs) -> LightningModule:
     """
     Loads the best model for the provided model type from the mlflow registry.
     :param model: model type
+    :param load_pretrained_on_sc: whether to load the best pretrained model on the SC dataset
+        or the best fully-trained model
     :param init_kwargs: keyword arguments forwarded to the class method LightningModule.load_from_checkpoint,
         which itself forwards the ones that it doesn't use to initialize the model.
         TODO since this implementation is kinda dirty, some provided kwargs may be overridden, see the code
     :return:
     """
     model_name = env.get_registered_model_name(model)
-    # alias = env.BEST_TUNED_MODEL_ALIAS
-    alias = env.BEST_FULL_TRAINED_MODEL_ALIAS
+    alias = env.get_dataset_specific_best_model_alias(dataset=Dataset.SC_TRAIN_SEMEVAL_VAL, tuning_alias=True) \
+        if load_pretrained_on_sc \
+        else env.BEST_FULL_TRAINED_MODEL_ALIAS
 
     mlflow.set_tracking_uri(env.MLFLOW_TRACKING_URI)
     client = mlflow.tracking.MlflowClient(tracking_uri=env.MLFLOW_TRACKING_URI)
