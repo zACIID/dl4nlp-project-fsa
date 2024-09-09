2dd331781e37d1855b31f39df68daf70ca62af04
zACIID <marchioropierluigi@gmail.com>
2024-09-09 23:25:29 +0200

    fix: fixed mlflow run name in evaluate_end_to_end.py

--- src/end_to_end_TODO/evaluation/evaluate_end_to_end.py
+++ src/end_to_end_TODO/evaluation/evaluate_end_to_end.py
@@ -89,6 +89,6 @@ if __name__ == '__main__':
     mlflow.set_experiment(env.EVALUATION_EXPERIMENT_NAME_PREFIX)
     with mlflow.start_run(
             log_system_metrics=True,
-            run_name=f"{datetime.now().isoformat(timespec='seconds')}-{loader.Model.HAND_ENG_MLP}-evaluation"
+            run_name=f"{datetime.now().isoformat(timespec='seconds')}-{loader.Model.END_TO_END}-evaluation"
     ) as run:
         _main()
